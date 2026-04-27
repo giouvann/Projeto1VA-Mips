3b0507f2592fa95488afa7af9316eca75d3ae5a4
Giovanna Costa <162203938+giouvann@users.noreply.github.com>
2026-04-27 09:45:00 -0300

    Create strcpy.asm

---
+++ exercicios/strcpy.asm
@@ -0,0 +1,9 @@
+######################################################################################
+# GRUPO: David Fernando, Evelin Dionizio, Giovanna Costa, Miguel Monteiro 
+# ATIVIDADE: Projeto 01 - 1ª VA 
+# DISCIPLINA: Arquitetura e Organização de Computadores 
+# SEMESTRE: 2026.1 
+# QUESTÃO:  Função strcpy 
+# DESCRIÇÃO: 
+######################################################################################
+
