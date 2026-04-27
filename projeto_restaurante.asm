e44af0447a02ad3a6d86b6d6d15ad57bf07ba504
Giovanna Costa <162203938+giouvann@users.noreply.github.com>
2026-04-27 10:11:40 -0300

    Delete projeto_restaurante.asm

--- projeto_restaurante.asm
+++
@@ -1,8 +0,0 @@
-######################################################################################
-# GRUPO: David Fernando, Evelin Dionizio, Giovanna Costa, Miguel Monteiro 
-# ATIVIDADE: Projeto 01 - 1ª VA
-# DISCIPLINA: Arquitetura e Organização de Computadores 
-# SEMESTRE: 2026.1 
-# QUESTÃO: Projeto Principal 
-# DESCRIÇÃO: Loop Principal/Shell
-######################################################################################
