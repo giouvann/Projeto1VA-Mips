068430279abb243bc3b95161bedc593de9686e77
giouvann <giovanniscostis@gmail.com>
2026-04-29 16:22:15 -0300

    Implementação e testes da função memcpy

--- exercicios/main_testes.asm
+++ exercicios/main_testes.asm
@@ -7,25 +7,54 @@
 # DESCRICAO: Codigo main para validar as funcoes strcpy, memcpy, strcmp, strncmp e strcat.
 ######################################################################################
 .data
-    # Dados para o teste da strcpy
-    string_origem:  .asciiz "Item: Picanha"  # String que queremos copiar
-    buffer_destino: .space 30                # Reserva 30 bytes vazios para o destino
+	# Dados para o teste
+	string_origem:  .asciiz "Item: Picanha"  # String que queremos copiar
+	buffer_destino: .space 30                # Reserva 30 bytes vazios para o destino
 
 .text
 .globl main
 
 main:
-    # Prepara os argumentos para a strcpy 
-    la $a0, buffer_destino    # $a0 = destino
-    la $a1, string_origem     #$a1 = origem
-    
-    jal strcpy         # Salta para a função strcpy (em outro arquivo) e salva o endereço de retorno em $ra
-    
-    # Mostra o resultado na tela
-    move $a0, $v0      # Move o valor de retorno ($v0 contém o endereço do destino) para $a0 para impressão
-    li $v0, 4          # Prepara o sistema para imprimir uma string (Serviço 4)
-    syscall
+	#jal teste_strcpy
+	jal teste_memcpy
+	
+	# Finaliza o programa após o teste escolhido
+	li $v0, 10		# Escolhe a função de "encerrar programa" (serviço 10)			
+	syscall			# Finaliza a execução
 
-    # Finaliza o programa 
-    li $v0, 10	      #	Escolhe a função de "encerrar programa" (serviço 10)			
-    syscall           # Finaliza a execução
+teste_strcpy:
+	addi $sp, $sp, -4	# Salva o endereço de retorno na pilha
+	sw $ra, 0($sp)		# Armazena o endereço de volta pro main
+	
+	la $a0, buffer_destino	# $a0 = destino
+	la $a1, string_origem	# $a1 = origem
+	jal strcpy		# chama a função strcpy
+	
+	# Imprime o resultado na tela
+	move $a0, $v0		# Move o valor de retorno ($v0 contém o endereço do destino) para $a0 para impressão
+	li $v0, 4		# Prepara o sistema para imprimir uma string (Serviço 4)
+	syscall
+	
+	lw $ra, 0($sp)		# Recupera endereço de retorno
+	addi $sp, $sp, 4	# Fecha o espaço na pilha
+	jr $ra			# Retorna para o main
+ 
+teste_memcpy:
+	addi $sp, $sp, -4	# Salva o endereço de retorno na pilha
+	sw $ra, 0($sp)		# Armazena o endereço de volta pro main
+
+	la $a0, buffer_destino	# Destino
+	la $a1, string_origem	# Origem
+	li $a2, 14		# Define a quantidade de bytes que quer copiar (Item: Picanha + \0)
+	jal memcpy		# Chama a função memcpy
+    
+	# Imprime o resultado na tela
+	move $a0, $v0		# Move o valor de retorno ($v0 contém o endereço do destino) para $a0 para impressão
+	li $v0, 4		# Prepara o sistema para imprimir uma string (Serviço 4)
+	syscall
+	
+	lw $ra, 0($sp)		# Recupera endereço de retorno
+	addi $sp, $sp, 4	# Fecha o espaço na pilha
+	jr $ra			# Retorna para o main
+
+    
--- exercicios/memcpy.asm
+++ exercicios/memcpy.asm
@@ -1,9 +1,25 @@
 ######################################################################################
 # GRUPO: David Fernando, Evelin Dionizio, Giovanna Costa, Miguel Monteiro 
-# ATIVIDADE: Projeto 01 - 1Âª VA
-# DISCIPLINA: Arquitetura e OrganizaÃ§Ã£o de Computadores 
+# ATIVIDADE: Projeto 01 - 1a VA
+# DISCIPLINA: Arquitetura e Organização de Computadores 
 # SEMESTRE: 2026.1 
-# QUESTÃƒO: FunÃ§Ã£o memcpy  
-# DESCRIÃ‡ÃƒO: 
+# QUESTÃO: Questão 1 - Funsão memcpy  
+# DESCRIÇÃO: Copia um bloco contínuo de memória de um endereço de origem para um endereço de destino
 ######################################################################################
+.globl memcpy
 
+memcpy:
+	move $v0, $a0		# Salva o endereço inicial de $a0 para $v0
+	
+loop:
+	beq $a2, $zero, end	# Se $a2 == 0, pula para o fim
+	lbu $t0, 0($a1)		# Carrega 1 byte da memoria de origem ($a1) para $t0
+	sb $t0, 0($a0)		# Armazena esse byte no endereço de destino
+	
+	addi $a0, $a0, 1	# Avança o ponteiro de destino para o proximo byte
+	addi $a1, $a1, 1	# Avança o ponteiro de origem para o proximo byte
+	addi $a2, $a2, -1	# decrementa o contador de bytes restantes (num --)
+	
+	j loop			# Continua o loop
+end:
+	jr $ra 			# Retorno
