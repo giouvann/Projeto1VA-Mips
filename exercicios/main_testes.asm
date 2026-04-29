35ae5a5ff4ef779707cd260e41d9bea9eca741fb
Miguel Monteiro <miguellmonteiro.5@gmail.com>
2026-04-29 17:07:32 -0300

    Correção de erro de sintaxe e organização dos testes por labels

--- exercicios/main_testes.asm
+++ exercicios/main_testes.asm
@@ -13,9 +13,9 @@
 
     # Dados para o teste da strcat
     str_destino:   .asciiz "Ola "
-                   .space 20         # Espanssão extra para caber o "Mundo!"
+                   .space 20         # EspanssÃ§o extra para caber o "Mundo!"
     str_origem:    .asciiz "Mundo!"
-    msg_strcat:    .asciiz "\nTeste strcat (Concatenação): "
+    msg_strcat:    .asciiz "\nTeste strcat (ConcatenaÃ§Ã£o): "
 
 .text
 .globl main
@@ -25,48 +25,48 @@ main:
 	#jal teste_memcpy
 	jal teste_strcat
 
-	# Finaliza o programa após o teste escolhido
-	li $v0, 10		# Escolhe a função de "encerrar programa" (serviço 10)
-	syscall			# Finaliza a execução
+	# Finaliza o programa apÃ³s o teste escolhido
+	li $v0, 10		# Escolhe a funÃ§Ã£o de "encerrar programa" (serviÃ§o 10)
+	syscall			# Finaliza a execuÃ§Ã£o
 
 teste_strcpy:
-	addi $sp, $sp, -4	# Salva o endereço de retorno na pilha
-	sw $ra, 0($sp)		# Armazena o endereço de volta pro main
+	addi $sp, $sp, -4	# Salva o endereÃ§o de retorno na pilha
+	sw $ra, 0($sp)		# Armazena o endereÃ§o de volta pro main
 
 	la $a0, buffer_destino	# $a0 = destino
 	la $a1, string_origem	# $a1 = origem
-	jal strcpy		# chama a função strcpy
+	jal strcpy		# chama a funÃ§Ã£o strcpy
 
 	# Imprime o resultado na tela
-	move $a0, $v0		# Move o valor de retorno ($v0 contém o endereço do destino) para $a0 para impressão
-	li $v0, 4		# Prepara o sistema para imprimir uma string (Serviço 4)
+	move $a0, $v0		# Move o valor de retorno ($v0 contÃªm o endereÃ§o do destino) para $a0 para impressÃ£o
+	li $v0, 4		# Prepara o sistema para imprimir uma string (ServiÃ§o 4)
 	syscall
 
-	lw $ra, 0($sp)		# Recupera endereço de retorno
-	addi $sp, $sp, 4	# Fecha o espaço na pilha
+	lw $ra, 0($sp)		# Recupera endereÃ§o de retorno
+	addi $sp, $sp, 4	# Fecha o espaÃ§o na pilha
 	jr $ra			# Retorna para o main
 
 teste_memcpy:
-	addi $sp, $sp, -4	# Salva o endereço de retorno na pilha
-	sw $ra, 0($sp)		# Armazena o endereço de volta pro main
+	addi $sp, $sp, -4	# Salva o endereÃ§o de retorno na pilha
+	sw $ra, 0($sp)		# Armazena o endereÃ§o de volta pro main
 
 	la $a0, buffer_destino	# Destino
 	la $a1, string_origem	# Origem
 	li $a2, 14		# Define a quantidade de bytes que quer copiar (Item: Picanha + \0)
-	jal memcpy		# Chama a função memcpy
+	jal memcpy		# Chama a funÃ§Ã£o memcpy
 
 	# Imprime o resultado na tela
-	move $a0, $v0		# Move o valor de retorno ($v0 contém o endereço do destino) para $a0 para impressão
-	li $v0, 4		# Prepara o sistema para imprimir uma string (Serviço 4)
+	move $a0, $v0		# Move o valor de retorno ($v0 contÃªm o endereÃ§o do destino) para $a0 para impressÃ£o
+	li $v0, 4		# Prepara o sistema para imprimir uma string (ServiÃ§o 4)
 	syscall
 
-	lw $ra, 0($sp)		# Recupera endereço de retorno
-	addi $sp, $sp, 4	# Fecha o espaço na pilha
+	lw $ra, 0($sp)		# Recupera endereÃ§o de retorno
+	addi $sp, $sp, 4	# Fecha o espaÃ§o na pilha
 	jr $ra			# Retorna para o main
 
 teste_strcat:
-        addi $sp, $sp, -4	# Salva o endereço de retorno na pilha
-        sw $ra, 0($sp)		# Armazena o endereço de volta pro main
+        addi $sp, $sp, -4	# Salva o endereÃ§o de retorno na pilha
+        sw $ra, 0($sp)		# Armazena o endereÃ§o de volta pro main
 
         la $a0, str_destino    # Carrega base do destino
         la $a1, str_origem     # Carrega origem
@@ -78,9 +78,9 @@ teste_strcat:
         syscall
 
         li $v0, 4              # ServiÃ§o 4: Imprimir String
-        la $a0, str_destino    # Carrega o endereço da string que agora deve ter
-        syscal
+        la $a0, str_destino    # Carrega o endereÃ§o da string que agora deve ter
+        syscall
 
-        lw $ra, 0($sp)		# Recupera endereço de retorno
-        addi $sp, $sp, 4	# Fecha o espaço na pilha
+        lw $ra, 0($sp)		# Recupera endereÃ§o de retorno
+        addi $sp, $sp, 4	# Fecha o espaÃ§o na pilha
         jr $ra			# Retorna para o main
