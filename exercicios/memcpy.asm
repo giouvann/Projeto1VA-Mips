0e7ed5d3558a1f856734e674cafb65b3440c48c8
Miguel Monteiro <miguellmonteiro.5@gmail.com>
2026-05-07 09:44:11 -0300

    Fix encoding issues in memcpy.asm comments

--- exercicios/memcpy.asm
+++ exercicios/memcpy.asm
@@ -1,23 +1,23 @@
 ######################################################################################
 # GRUPO: David Fernando, Evelin Dionizio, Giovanna Costa, Miguel Monteiro 
 # ATIVIDADE: Projeto 01 - 1a VA
-# DISCIPLINA: Arquitetura e Organiza��o de Computadores 
+# DISCIPLINA: Arquitetura e Organização de Computadores 
 # SEMESTRE: 2026.1 
-# QUEST�O: Quest�o 1 - Funs�o memcpy  
-# DESCRI��O: Copia um bloco cont�nuo de mem�ria de um endere�o de origem para um endere�o de destino
+# QUESTÄO: Questão 1 - Funsäo memcpy  
+# DESCRIÇÃO: Copia um bloco contínuo de meméria de um endereço de origem para um endereço de destino
 ######################################################################################
 .globl memcpy
 
 memcpy:
-	move $v0, $a0		# Salva o endere�o inicial de $a0 para $v0
+	move $v0, $a0		# Salva o endereço inicial de $a0 para $v0
 	
 loop_memcpy:
 	beq $a2, $zero, end_memcpy	# Se $a2 == 0, pula para o fim
 	lbu $t0, 0($a1)		# Carrega 1 byte da memoria de origem ($a1) para $t0
-	sb $t0, 0($a0)		# Armazena esse byte no endere�o de destino
+	sb $t0, 0($a0)		# Armazena esse byte no endereço de destino
 	
-	addi $a0, $a0, 1	# Avan�a o ponteiro de destino para o proximo byte
-	addi $a1, $a1, 1	# Avan�a o ponteiro de origem para o proximo byte
+	addi $a0, $a0, 1	# Avança o ponteiro de destino para o proximo byte
+	addi $a1, $a1, 1	# Avança o ponteiro de origem para o proximo byte
 	addi $a2, $a2, -1	# decrementa o contador de bytes restantes (num --)
 	
 	j loop_memcpy			# Continua o loop
